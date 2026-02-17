aabb8d3014741ab22cab6cd3068e53e38b42c6a6
Caleb00004 <calebakpan7@gmail.com>
2026-02-17 13:10:28 -0800

    update

--- components/custom-bottom-nav.tsx
+++ components/custom-bottom-nav.tsx
@@ -16,7 +16,7 @@ export default function CustomTabBar({
 }: BottomTabBarProps) {
   return (
     <View style={styles.container}>
-        <View style={{height: 3, width: 20, position: "absolute", backgroundColor: "#5653FC"}}></View>
+        <View style={styles.indicator}></View>
       {state.routes.map((route, index) => {
         const isFocused = state.index === index;
 
@@ -38,13 +38,13 @@ export default function CustomTabBar({
         const title = options.title ?? route.name;
 
         return (
-          <Pressable key={route.key} onPress={onPress} style={[{...styles.tab}, isFocused ? {borderTopColor: "#5653FC", borderTopWidth: 5} : {}]}>
+          <Pressable key={route.key} onPress={onPress} style={[{...styles.tab}]}>
             <Icon
               width={24}
               height={24}
               color={isFocused ? "#5653FC" : "#717680"}
             />
-            <ThemedText style={{fontSize: 12, color: isFocused ? "#5653FC" : "#717680"}}>{title}</ThemedText>
+            <ThemedText style={{fontSize: 12,marginTop: 3, color: isFocused ? "#5653FC" : "#717680"}}>{title}</ThemedText>
           </Pressable>
         );
       })}
@@ -53,23 +53,34 @@ export default function CustomTabBar({
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    position: "relative",
-    height: 80,
-    // gap: 12,
-    paddingHorizontal: 20,
-    backgroundColor: "#fff",
-    // borderTopWidth: 1,
-    borderTopColor: "#eee",
-    justifyContent: "space-around",
-    alignItems: "center",
-  },
-  tab: {
-    flex: 1,
-    height: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-    // backgroundColor: "red"
-  },
+    container: {
+        flexDirection: "row",
+        position: "relative",
+        height: 80,
+        // gap: 12,
+        paddingHorizontal: 20,
+        elevation: 20,
+        backgroundColor: "#fff",
+        justifyContent: "space-around",
+        alignItems: "center",
+    },
+    tab: {
+        flex: 1,
+        height: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+        // backgroundColor: "red"
+    },
+    indicator: {
+        position: "absolute", 
+        height: 4,
+        width: "17%",
+        borderBottomRightRadius: 10,
+        borderBottomLeftRadius: 10,  
+        backgroundColor: "#5653FC",
+        top: 0,
+        // left: "13%"
+        left: 0,
+        transform: [{translateX: "80%"}]
+    }
 });
